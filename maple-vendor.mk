6a682a2d51d82863872635c3be1ba2aaf0336558
Chattrapat Sangmanee <aomsin27@hotmail.co.th>
2024-10-20 19:06:03 +0700

    aomsin

--- maple-vendor.mk
+++ maple-vendor.mk
@@ -282,7 +282,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/maple/proprietary/vendor/bin/sensors.qti:$(TARGET_COPY_OUT_VENDOR)/bin/sensors.qti \
     vendor/sony/maple/proprietary/vendor/bin/ta_qmi_service:$(TARGET_COPY_OUT_VENDOR)/bin/ta_qmi_service \
     vendor/sony/maple/proprietary/vendor/bin/taimport_vendor:$(TARGET_COPY_OUT_VENDOR)/bin/taimport_vendor \
-    vendor/sony/maple/proprietary/vendor/bin/thermal-engine:$(TARGET_COPY_OUT_VENDOR)/bin/thermal-engine \
     vendor/sony/maple/proprietary/vendor/bin/time_daemon:$(TARGET_COPY_OUT_VENDOR)/bin/time_daemon \
     vendor/sony/maple/proprietary/vendor/camera/SEM13BS1/color_ctrl.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/color_ctrl.dat \
     vendor/sony/maple/proprietary/vendor/camera/SEM13BS1/dataflow.dat:$(TARGET_COPY_OUT_VENDOR)/camera/SEM13BS1/dataflow.dat \
@@ -393,7 +392,6 @@ PRODUCT_COPY_FILES += \
     vendor/sony/maple/proprietary/vendor/etc/sony_effect/znr_param_48k_NoCam.tbl:$(TARGET_COPY_OUT_VENDOR)/etc/sony_effect/znr_param_48k_NoCam.tbl \
     vendor/sony/maple/proprietary/vendor/etc/sony_effect/znr_param_48k_SubSemCam.tbl:$(TARGET_COPY_OUT_VENDOR)/etc/sony_effect/znr_param_48k_SubSemCam.tbl \
     vendor/sony/maple/proprietary/vendor/etc/sony_effect/znr_param_48k_SubSoiCam.tbl:$(TARGET_COPY_OUT_VENDOR)/etc/sony_effect/znr_param_48k_SubSoiCam.tbl \
-    vendor/sony/maple/proprietary/vendor/etc/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     vendor/sony/maple/proprietary/vendor/etc/wifi/bdwlan.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/bdwlan.bin \
     vendor/sony/maple/proprietary/vendor/firmware/a540_zap.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/a540_zap.b01 \
     vendor/sony/maple/proprietary/vendor/firmware/a540_zap.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/a540_zap.mdt \
